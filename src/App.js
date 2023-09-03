7b3ce215504af666d6dd0145b7551612d3479ee2
maurocas88 <maurocas88@hotmail.com>
2023-09-03 01:46:01 -0300

    Primeros pasos con React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Header_RC from './Components/header_C/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header_RC/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          A ver como va la cosa
         </p>
         <a
           className="App-link"
---
+++ src/Components/header_C/Header.js
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+function Header_RC () {
+    return <header className="header">
+        <img src="/img/header_I.png" alt= "org"/>;
+    </header>
+}
+
+export default Header_RC;
